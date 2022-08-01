78687c313271ef140e604335a7fee39cbed5bc36
Abdul Kader <80806477+kaderabdul79@users.noreply.github.com>
2022-08-01 06:39:25 +0600

    converted html template for backend has been finished

---
+++ src/components/backend/Sidebar.vue
@@ -0,0 +1,74 @@
+<template>
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+<!-- Sidebar - Brand -->
+<a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+  <div class="sidebar-brand-icon rotate-n-15">
+    <i class="fas fa-laugh-wink"></i>
+  </div>
+  <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+</a>
+
+<!-- Divider -->
+<hr class="sidebar-divider my-0">
+
+<!-- Nav Item - Dashboard -->
+<li class="nav-item active">
+  <a class="nav-link" href="../../public/backend/index.html">
+    <i class="fas fa-fw fa-tachometer-alt"></i>
+    <span>Dashboard</span></a>
+</li>
+
+<!-- Divider -->
+<hr class="sidebar-divider">
+
+<!-- Heading -->
+<div class="sidebar-heading">
+  Interface
+</div>
+
+<!-- Nav Item - Pages Collapse Menu -->
+<li class="nav-item">
+  <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+    <i class="fas fa-fw fa-cog"></i>
+    <span>Components</span>
+  </a>
+  <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+    <div class="bg-white py-2 collapse-inner rounded">
+      <router-link class="collapse-item" to="/">Buttons</router-link>
+    </div>
+  </div>
+</li>
+
+<!-- Divider -->
+<hr class="sidebar-divider">
+
+<!-- Heading -->
+<div class="sidebar-heading">
+  Addons
+</div>
+
+<!-- Nav Item - Pages Collapse Menu -->
+<li class="nav-item">
+  <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
+    <i class="fas fa-fw fa-folder"></i>
+    <span>Pages</span>
+  </a>
+  <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+    <div class="bg-white py-2 collapse-inner rounded">
+      <router-link class="collapse-item" to="/">Buttons</router-link>
+    </div>
+  </div>
+</li>
+</ul>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/backend/HomeView.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="page-top">
+<!-- Page Wrapper -->
+<div id="wrapper">
+
+  <!-- Sidebar -->
+      <Sidebar />
+  <!-- End of Sidebar -->
+
+  <!-- Content Wrapper -->
+  <div id="content-wrapper" class="d-flex flex-column">
+
+    <!-- Main Content -->
+    <div id="content">
+
+      <!-- Topbar -->
+        <Topbar />
+      <!-- End of Topbar -->
+
+      <!-- Begin Page Content -->
+        <MainContent />
+      <!-- /.container-fluid -->
+
+    </div>
+    <!-- End of Main Content -->
+
+    <!-- Footer -->
+      <Footer />
+    <!-- End of Footer -->
+
+  </div>
+  <!-- End of Content Wrapper -->
+
+</div>
+<!-- End of Page Wrapper -->
+
+
+  </div>
+</template>
+
+<script>
+import Sidebar from '../../components/backend/Sidebar.vue';
+import Topbar from '../../components/backend/Topbar.vue';
+import MainContent from '../../components/backend/MainContent.vue';
+import Footer from '../../components/backend/Footer.vue';
+export default {
+    components: { Sidebar, Topbar, MainContent, Footer }
+}
+</script>
+
+<style>
+
+</style>
